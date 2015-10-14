4d16a8537c4f412ad06eb8331816af48394c5a1c
Kevin Nash <kjn33@case.edu>
2015-10-14 16:32:00 -0400

    Assignment 7 first step

---
+++ pa7/circular_array.py
@@ -0,0 +1,22 @@
+# Kevin Nash (kjn33)
+# EECS 293
+# Assignment 7
+
+class CircularArray:
+    """ """
+
+    def __init__(self, k):
+        """ """
+        self.a = [None for _ in xrange(k)]
+        self.head = 0
+
+    def offer(self, x):
+        """ """
+        self.a[self.head % len(self.a)] = x
+        self.head += 1
+
+    def maximum(self):
+        """ """
+        if self.head > 0:
+            return max(self.a[0:min(self.head, len(self.a))])
+        return None
